eaf2b28f5d9f2b5694f6b47ec3c8d93f53f0f5aa
JohnyJD <kontakt.jan.dunaj@gmail.com>
2021-03-22 09:16:50 +0100

    init commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener {
+            Toast.makeText(
+                this,
+                getString(R.string.buttonToast),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        val hey : Int = 100;
+    }
+}
